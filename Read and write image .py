6af69e30d1dc7ab567f7c62dac81d541bccaba2a
surajk-maurya <suraj.k.mourya@gmail.com>
2019-11-21 22:05:12 +0530

    New learning

--- Read and write image .py
+++ Read and write image .py
@@ -3,9 +3,11 @@ img=cv2.imread('lena.jpg',1) #read lena.jpg in: greyscale=0, colour=1, unchanged
 
 print(img)                   #print the image in matrix form
 
-cv2.imshow('image',img)      #show 'img' variable in image form
+cv2.imshow('image',img)      #show 'img' variable in image form to wait the the program for 5 sec at the image window
 
-cv2.waitKey(5000)            #to wait the the program for 5 sec at the image window
-
-cv2.imwrite('lena_copy.png',img) #copy  the 'img' variable in in png formate
+k=cv2.waitKey() & 0xFF
+if k==27:
+ cv2.destreoyAllwindows()
+elif k== ord('s'):
+ cv2.imwrite('lena_copy.png',img) #copy  the 'img' variable in in png formate
 
